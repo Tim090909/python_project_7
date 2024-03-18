a5352c83e16ac3d2a0ccb2a752c1781e362c8b4d
Tim090909 <t.hrynchuk@ukma.edu.ua>
2024-03-18 14:25:08 +0200

    added tests for input via pandas

--- tests/input_test.py
+++ tests/input_test.py
@@ -7,10 +7,16 @@ class TestInputFunctions(unittest.TestCase):
         self.assertEqual(text, "Hello. It works!")
 
     def test_input_from_file_builtin_2(self):
-        text = input_from_file_builtin("test_data/non_existing_test_input.txt")
+        text = input_from_file_builtin("non_existing_test_input.txt")
         self.assertEqual(text, "File not found.")
 
+    def test_input_from_file_pandas_1(self):
+        text = input_from_file_pandas("test_data/test_input.txt")
+        self.assertEqual(text, "Hello. It works!")
 
+    def test_input_from_file_pandas_2(self):
+        text = input_from_file_pandas("non_existing_test_input.txt")
+        self.assertEqual(text, "File not found.")
 
 
 if __name__ == '__main__':
